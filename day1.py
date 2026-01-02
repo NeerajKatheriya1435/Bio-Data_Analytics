68d9fdff57b49a26983ccc8f392b07f4093a9d5b
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-02 18:43:46 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,14 @@
+"""
+print("Neeraj Katheriya")
+create excel 
+print("Neeraj Katheriya I am good")
+sdfghj
+ghjk
+fghjk
+ghjk
+This is multiline comment
+"""
+'''
+dfghnjm,k
+'''
+print("Neeraj\t\t\tKatheriya's \\neeraj \\nDhruv's\" is good")

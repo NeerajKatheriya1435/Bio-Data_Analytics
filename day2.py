f2c2c10430206e0b6003aa17ab363d6b580dbf71
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-03 11:26:53 +0530

    Inputs and data types

---
+++ day2.py
@@ -0,0 +1,77 @@
+# name="Harish chndra gupta"
+# dhruv=1234567
+# print(name)
+# print("Hello Dhruv")
+# print(dhruv)
+
+# dhruvarhyujjyujygarwal="chini"
+# print(dhruvagarwal)
+
+# varaible1="Shubham"
+# kisibhiname="Priti"
+
+# # arunkumar="good" # right method
+# # arun kumar="good" # wrong method to create variable
+
+# shubh@m123="Shubham" # (a-z,A-Z,0-9,_)
+# # 345shubhm="Shubham"
+# _shubhm="Shubham1234"  # varible initilization
+# print(_shubhm)
+
+# print(varaible1)
+# print(kisibhiname)
+
+# var1="Rekha"
+# VAR1="Neha"
+# print(var1)
+
+# int neha=89
+
+# num1=56
+# print(num1)
+# print(type(num1))
+
+# num2=56.67
+# print(num2)
+# print(type(num2))
+
+# str1="Dhruv Kumar"
+# print(str1)
+# print(type(str1))
+# num2=56.67
+# print(num2)
+# print(type(num2))
+
+# haveYouSubmittedFee=True
+# print(haveYouSubmittedFee)
+# print(type(haveYouSubmittedFee))
+
+# a=8
+# b=7
+
+# num1=6
+# num2=4
+
+# input1=input("Enter your number 1: ")
+# input2=input("Enter your number 2: ")
+
+# print("This my input 1 value")
+# print(input1)
+# print("This my input 2 value")
+# print(input2)
+
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+
+# print("Shubham","Aman","Rohan",sep=" ")
+# print(6+8)
+# print("Shubham","Aman")
+
+FisrtName=input("Enter Your FisrtName: ")
+LastName=input("Enter Your LastName: ")
+
+print("My name is",FisrtName,LastName)
+
+

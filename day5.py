2745e72273cf86bc01f94eefc69434e6d8daa8dc
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-07 18:33:17 +0530

    match case

---
+++ day5.py
@@ -0,0 +1,55 @@
+# a=7
+# b=7
+# if(a<=b):
+#     print("hello i am good1")
+#     print("hello i am good2")
+#     print("hello i am good2")
+#     print("hello i am good4")
+# elif(a==b):
+#     print("Good is not good")
+# else:
+#     print("Not Fine1")
+#     print("Not Fine2")
+# print("Good sir")
+
+# inp1=5
+# inp2=8
+# str1=input("Enter the operator")
+
+# match(str1):
+#     case "+":
+#         print(inp1+inp2)
+#     case "-":
+#         print(inp1-inp2)
+#     case 33:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case _:
+#         print("Please input Number between 1 to 4")
+
+# print("1")
+# print("2")
+# print("3")
+# print("4")
+
+# for i in range(6,11,3):
+# for i in range(6,11,3):
+#     print(i)
+
+# string1="Dhruv Agrwal"
+# print(string1)
+# for item in string1:
+#     print(item)
+
+# for item in range(11):
+#     # if(item==4):
+#     #     break
+#     if(item%2==0):
+#         continue
+#     print(item)
+
+# print("hello")
+
+
+

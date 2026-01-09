d7a87c6a2a6926efdf588f43a8949374b34cce50
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-09 18:42:20 +0530

    list and function

---
+++ day6.py
@@ -0,0 +1,21 @@
+
+
+
+# for item in range(5,11):
+#     print(item)
+
+
+# fact=1
+
+# inp1=5
+# for item in range(1,inp1+1):
+#     fact=fact*item
+
+# print(fact)
+
+for i in range(1,10):
+    # if(i==5):
+    #     break
+    if(i%2==0):
+        continue
+    print(i)
---
+++ day7.py
@@ -0,0 +1,78 @@
+
+# inp1=4
+# inp2=7
+# print(inp1+inp2)
+
+# inp3=8
+# inp4=7
+# print(inp3+inp4)
+
+# def addTwoNum(num1,num2):
+#     print("Hello Good Morning")
+#     print(num1+num2)
+#     print("Hello Good After")
+#     print("Hello Good Evening")
+
+
+# def greet():
+#     print("Hello hii Dhruv")
+
+# addTwoNum(5,7)
+# addTwoNum(4,19)
+# greet()
+# greet()
+# greet()
+# addTwoNum(15,7)
+
+# def addTwoNum(num1,num2): # Formal Parameters
+#     # print("Hello Good Morning")
+#     # print(num1+num2)
+#     return num1+num2
+
+
+# addTwoNum(5,7) # actual parmeters
+# addTwoNum(4,7)
+# print(addTwoNum(6,9))
+# def greet():
+#     print("Hello sir")
+
+# greet()
+
+# str1="Dhruv"
+# print(len(str1))
+
+# list1=[23,33,14,24,56,53,32,34]
+# list2=[23,33,14,24,56,53,32,34,"Hello",True,7.9087]
+
+# print(list1[0])
+# print(list1[1])
+# print(list1)
+# print(list2)
+
+# list1[2]="Rohan"
+# list1[2]=56
+# print(list1)
+
+# print(list1[2:6])
+# print(list1[:6])
+# print(list1[:])
+# print(list1[1:6:2])
+# print(list1[-3])
+# print(list1[-3:-1])
+
+# list1=[23,33,14,24,56,53,32,34]
+# list1.sort(reverse=True)
+# list1.sort()
+# print(list1)
+
+list1=[23,33,14,24]
+list2=[56,53,32,34]
+# list1.reverse()
+# print(list1)
+# print(list1.index(33))
+# list1.insert(2,56)
+# print(list1)
+# x=list1.copy()
+# print(list1.count(32))
+# print(x)
+print(list1+list2)

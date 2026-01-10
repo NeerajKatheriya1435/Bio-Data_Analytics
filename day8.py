6cfbaa0da2221b5401297b54f77eca870be46594
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-10 18:22:32 +0530

    Python list tuple sets

---
+++ day8.py
@@ -0,0 +1,71 @@
+
+# list1=["Aman","Rohan","Karan"]
+# list1[1]="Suman"
+# list1.append("Dhruv")
+# print(list1)
+
+# tup1=(4,6,7,8,7,7)
+# print(type(tup1))
+
+# tup1[1]=7
+
+# print(tup1[1:3])
+# print(tup1[-1])
+
+# l1=list(tup1)
+
+# l1.append(89)
+
+# tup1=tuple(l1)
+
+# print(tup1)
+# print(tup1.count(7))
+# print(tup1.index(6))
+
+# list1=[6,8]
+# list1=[1]
+# print(list1)
+# print(type(list1))
+
+# list1=(1,6,7)
+# list1=()
+# list1=(5,)
+# print(list1)
+# print(type(list1))
+
+# t1={5,7,3,7,5,6,7,8,8}
+# print(t1)
+
+# l1=[6,8,5,"dfghj",[6,8,9,9],(6,7,8)]
+
+
+# s2={3,4,5,6,"Rohan",(5,7,8)}
+
+# s1={1,2,3,4}
+
+# s2={3,4,5,6}
+# s2.add(34)
+# s2.remove(15)
+# s2.discard(5)
+
+# s2.pop()
+# s2.clear()
+# print(s2)
+# s2.update([7,8,9,0])
+
+# print(s2)
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+
+# print(s1.union(s2))
+# print(s1.intersection(s2))
+# print(s1.difference(s2))
+# print(s2.difference(s1))
+# print(s1.symmetric_difference(s2))
+
+# if 43 in s1:
+#     print("Yes Present")
+
+# if 43 not in s1:
+#     print("No Present")

d964b246baa6f4ca53234133d87ef8feeacda37b
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-13 18:32:52 +0530

    till oops imn python

---
+++ day10.py
@@ -0,0 +1,53 @@
+# file1=open("example.txt","w")
+
+# print(file1.read())
+# print(file1.readline())
+# print(file1.readline())
+# print(file1.readline())
+# content=file1.readline()
+# print(content)
+
+# while(True):
+#     content=file1.readline()
+#     if not content:
+#         break
+#     print(content)
+
+
+
+# lines=["Hello","\nHanuman","\nRohan"]
+# file1.writelines(lines)
+file1=open("example.txt","w")
+# file1.seek(10)
+# file1.readline()
+# print(file1.tell())
+# print(file1.read())
+# file1.write("Siddharth kaise ho")
+# file1.truncate(10)
+
+# def addTwoNum(num1,num2):
+#     return (num1+num2)
+
+# print(addTwoNum(2,4))
+# print(addTwoNum(2,14))
+
+# add=lambda n1,n2:n1+n2
+# cube=lambda n1:n1*n1*n1
+
+# print(add(6,7))
+# print(cube(5))
+
+# l1=[7,4,3]
+# map1=list(map(lambda n1:n1+6,l1))
+# print(map1)
+
+# l1=[7,4,3,7,4,9,7]
+# map1=list(filter(lambda n1:n1>4,l1))
+# print(map1)
+
+# from functools import reduce
+# l1=[7,4,3,2]
+# map1=(reduce(lambda a,b:a+b,l1))
+# print(map1)
+
+

64deb611eb7b248956f1a23622c7ee9c68c6922f
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-28 18:42:29 +0530

    fasta files creations

---
+++ BioPython_Code/day3.py
@@ -0,0 +1,24 @@
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# merged_list = [*list1, *list2]
+# merged_list will be [1, 2, 3, 4, 5, 6]
+
+# b,f,a=list1
+# print(b)
+# print(f)
+# print(a)
+
+dict1={
+    "Name":"Shivam",
+    "Age":56,
+    "Occupation":"Doctor"
+}
+
+# print(dict1)
+
+item1,item2,item3=dict1.items()
+key,value=item1
+
+print("My key is: ",key)
+print("My Value is: ",value)
+# print(item1)
---
+++ BioPython_Code/day4.py
@@ -0,0 +1,16 @@
+
+# from Bio import SeqIO
+# from Bio.SeqRecord import SeqRecord
+
+# record=list(SeqIO.parse("file12.fasta","fasta"))
+
+# tup1=tuple(record)
+
+# print(record)
+# SeqIO.write(record,"newFasta.fasta","fasta")
+# print("File Created Successfully!!!")
+
+# f = open('myfile.txt', 'w')
+# lines = ['line 1 Abhishek\n', 'line 2 Amit\n', 'line 3 Suraj\n']
+# f.writelines(lines)
+# f.close()

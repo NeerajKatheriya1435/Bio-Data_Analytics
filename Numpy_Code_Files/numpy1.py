a4c578c374583de8e04914d065b1f530e113ba74
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-13 17:40:38 +0530

    araanged files

---
+++ Numpy_Code_Files/numpy1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import time
+
+# arr1=np.array([4,6,7,8])
+
+# start=time.time()
+# arr1=np.arange(100000000)*2
+# end=time.time()
+
+# print("Array seconds: ",end-start)
+
+# for i in arr1:
+#     print(i)
+
+
+# start=time.time()
+# l1=[i*2 for i in range(100000000)]
+# end=time.time()
+# print("List seconds: ",end-start)
+
+# for i in l1:
+#     print(i)
+
+arr1=np.random.randint(5,90,7)
+arr1=np.array([4,5,6,7])
+print(arr1)

6fa96852dfae0c0814a48fc2c7815e0438d9ce33
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-17 18:40:18 +0530

    added data

--- Numpy_Code_Files/numpy1.py
+++ Numpy_Code_Files/numpy1.py
@@ -21,6 +21,24 @@ import time
 # for i in l1:
 #     print(i)
 
-arr1=np.random.randint(5,90,7)
-arr1=np.array([4,5,6,7])
-print(arr1)
+# arr1=np.random.randint(5,90,7)
+# arr1=np.array([4,5,6,7])
+# print(arr1)
+
+# arr1=np.array([4,8,2,6])
+
+# arr2=np.copy(arr1)
+# arr2=arr1
+# arr2[1]=100
+
+# print(arr1)
+# print(arr2)
+
+# arr1=np.array([4,8,2,6])
+
+# arr4=arr1[1:3]
+
+# arr4[1]=90
+
+# print(arr1)
+# print(arr4)
